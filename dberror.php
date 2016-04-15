0152539887674cf436b9a9d46de23d1461305fe5
Samuel Agyemang <khermz2012@gmail.com>
2016-04-15 19:31:20 +0100

    Uploading to server

---
+++ dberror.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>eElectronics - HTML eCommerce Template</title>
+
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet'
+          type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/owl.carousel.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/responsive.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="./js/html5shiv.min.js"></script>
+    <script src="./js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body style="background-color: #1ABC9C">
+
+<div class="container text-center">
+    <div class="logo-404" style="height: 250px">
+        <!--<a href="index.twig"><img height="750px" width="650px" src="./img/404.png" alt=""/></a>-->
+    </div>
+
+    <div class="content-404">
+        <h1 style="color: #ffffff; font-size: 40px"><b>Sorry !</b></h1>
+        <h1 style="color: #ffffff">We are restocking.</h1>
+        <p style="font-size: 20px; color: #ffffff;;">Please come back later.</p>
+        <a href="index.php" class="btn btn-primary err">Retry</a>
+        <br>
+    </div>
+</div>
+
+
+<script src="../js/jquery.js"></script>
+<script src="../js/price-range.js"></script>
+<script src="../js/jquery.scrollUp.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/jquery.prettyPhoto.js"></script>
+<script src="../js/main.js"></script>
+</body>
+</html>
